987c42f044a9de5364fadcff080776136ec5ece5
Serg Mykhailov <s.mykhailov@olearis.com>
2019-06-24 01:43:18 +0300

    Main screen files were declared

---
+++ lib/Screens/StoreMainScreen.dart
@@ -0,0 +1,13 @@
+import 'package:nok_nok/Screens/StoreMainScreenState.dart';
+
+import 'package:flutter/cupertino.dart';
+
+class StoreMainScreen extends StatefulWidget {
+
+  StoreMainScreen({Key key})
+   : super(key: key);
+
+  @override
+  StoreMainScreenState createState() => StoreMainScreenState();
+
+}
---
+++ lib/Screens/StoreMainScreenState.dart
@@ -0,0 +1,15 @@
+import 'StoreMainScreen.dart';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+
+class StoreMainScreenState extends State<StoreMainScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      child: Text("Non Nok Store Content"),
+    );
+  }
+
+}
