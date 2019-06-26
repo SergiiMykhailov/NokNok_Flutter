b911a464b1a2d85f180e904635ed96b0f540abaa
Serg Mykhailov <s.mykhailov@olearis.com>
2019-06-27 00:58:44 +0300

    Main screen vertical layout was setup

--- lib/Screens/StoreMainScreenState.dart
+++ lib/Screens/StoreMainScreenState.dart
@@ -4,12 +4,34 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
 
 class StoreMainScreenState extends State<StoreMainScreen> {
-
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: Text("Non Nok Store Content"),
+      child: Center(
+        child: Row(
+          children: [
+            Container(
+              color: CupertinoColors.destructiveRed,
+              padding: EdgeInsets.only(left: 8, right: 8),
+              child:
+                Center(
+                  child: Text("Left column"),
+                ),
+            ),
+            Expanded(
+              child:
+                Container(
+                  color: CupertinoColors.activeGreen,
+                  padding: EdgeInsets.only(left: 8, right: 8),
+                  child:
+                    Center(
+                      child: Text("Right column"),
+                    ),
+              ),
+            )
+          ],
+        )
+      )
     );
   }
-
 }
